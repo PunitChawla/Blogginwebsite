3648c8aeb8beaf989cd0eac71c7738f263c20eca
PunitChawla <chawlapunit79@gmail.com>
2024-08-26 12:05:27 +0530

    add sigin Route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -63,4 +63,39 @@ router.post("/signup", async (req, res)=>{
     }
 })
 
+const signinSchema = zod.object({
+    username : zod.string().email(),
+    password : zod.string().min(6)
+})
+
+router.post("/signin", async (req, res)=>{
+
+    const responce = signinSchema.safeParse(req.body)
+    if(!responce.success)
+    {
+        return res.status(411).json({
+            msg : "Invalid input",
+            error : responce.error
+        })
+    }
+
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    });
+    if (user) {
+        const token = jwt.sign({
+            userId: user._id
+        }, JWT_SECRET);
+  
+        res.json({
+            token: token
+        })
+        return;
+    }
+    
+    res.status(411).json({
+        message: "Error while logging in"
+    })
+})
 module.exports = router
